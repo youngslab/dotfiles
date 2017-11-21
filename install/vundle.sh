6927f76af437866a647d920661f52372dbfccab5
Jaeyoung ParkLars Kappert <jaeyoungs.park@lge.com>
2017-11-22 03:13:04 +0900

    Supports bash on windows10

--- install/vundle.sh
+++ install/vundle.sh
@@ -1,4 +1,11 @@
-mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/VundleVim/Vundle.vim "$BUNDLE_DIR" || (cd "$BUNDLE_DIR" && git pull origin master))
+#!/usr/bin/env bash
+
+BUNDLE_DIR=~/.vim/bundle
+
+# Install/update Vundle
+
+mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/VundleVim/Vundle.vim "$BUNDLE_DIR"/Vundle.vim || (cd "$BUNDLE_DIR"/Vundle.vim  && git pull origin master))
+
 
 # Install bundles
 vim +PluginInstall +qall
