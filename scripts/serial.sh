9c142a9d17b05867d38f5a23396427c6b6c84429
Jaeyoung Park <jaeyoungs.park@gmail.com>
2021-01-15 10:00:05 +0900

    serial) multiple argument

--- scripts/serial.sh
+++ scripts/serial.sh
@@ -1,6 +1,4 @@
 
-
-
 # current pane id
 tmux_curr_pane_id(){
   tmux display-message -p '#I.#P'
@@ -19,7 +17,9 @@ dispatcher_set_server() {
 # $1: server, $2: command
 dispatcher_send_to() {
   # TODO: check server exists
-  tmux send-keys -t $(tmux showenv ${1} | sed "s:^.*=::") $2 Enter
+  server=$(tmux showenv ${1} | sed "s:^.*=::")
+  shift
+  tmux send-keys -t $server "$*" Enter
 }
 
 dispatcher_usage(){
@@ -33,14 +33,29 @@ SERIAL_SERVER_NAME="serial"
 
 serial_send() {
   # send command to
-  dispatcher_send_to ${SERIAL_SERVER_NAME} $1
+  dispatcher_send_to ${SERIAL_SERVER_NAME} "$@"
 }
 
 # set connection
 # ex) serial_set_conn [{winid}.{paneid}(optional)]
 serial_set_server() {
   # set env variable to expose
-  dispatcher_set_server ${SERIAL_SERVER_NAME} $1
+  dispatcher_set_server ${SERIAL_SERVER_NAME} "$@"
+}
+
+serial_fast() {
+  serial_send fast
+}
+
+serial_show_regulators(){
+  serial_send \
+		'cd /sys/class/regulator'
+
+  serial_send \
+		'echo "No Name min-voltage target-voltage max-voltage";i=3; while [ 1 ]; do \
+		if [ $i -gt 43 ]; then break; fi; echo "$i: $(cat ./regulator.$i/name) $(cat \
+			./regulator.$i/min_microvolts) <- $(cat ./regulator.$i/microvolts) -> \
+			$(cat ./regulator.$i/max_microvolts)"; i=$(($i+1)); done'
 }
 
 # usage
