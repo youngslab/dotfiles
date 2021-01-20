36ad55b450f9f8d87aa2f57d0f49c59d361e2244
Jaeyoung Park <jaeyoungs.park@gmail.com>
2021-01-21 02:53:30 +0900

    tmux) dispatcher) move pane

--- scripts/serial.sh
+++ scripts/serial.sh
@@ -1,68 +1,65 @@
 
-# current pane id
-tmux_curr_pane_id(){
-  tmux display-message -p '#I.#P'
-}
-
-tmux_curr_win_id(){
+tmux_curr_window_idx(){
   tmux display-message -p '#I'
 }
 
-tmux_pane_count(){
-  tmux list-panes | wc -l
+tmux_curr_pane_idx(){
+  tmux display-message -p '#P'
 }
 
-# $1 : src pane id. ex) 1.3
-# return its new pane id
+# current pane id
+tmux_curr_pane_id(){
+  tmux display-message -p '#{pane_id}'
+}
+
+# current window id
+tmux_curr_window_id(){
+  tmux display-message -p '#{window_id}'
+}
+
+# take pane to the current window
+# $1 : src pane_id. ex) %3
 tmux_take_pane(){
-	winid=$(tmux_curr_win_id)
-	paneid=$(tmux_curr_pane_id)
-	prev=$(tmux_pane_count)
-  tmux join-pane -s $1 -t $winid > /dev/null 2>&1
-	tmux select-pane -t $paneid > /dev/null 2>&1
-	curr=$(tmux_pane_count)
-	if [ $prev == $curr ]; then
-		echo -1.-1
-		return
-	fi
-	echo $winid.$curr
+  pane_id=$(tmux_curr_pane_id)
+  tmux join-pane -s $1 -t $(tmux_curr_window_id) > /dev/null 2>&1
+  tmux select-pane -t $pane_id
 }
 
 # server
-# $1: token
+# $1: servername
 dispatcher_set_server() {
-  if [ $2 ]; then
-    tmux setenv $1 $2
-    return
+  server_name=$1
+  pane_id=$2
+  if [ -z $pane_id ]; then
+    pane_id=$(tmux_curr_pane_id)
   fi
-  tmux setenv $1 $(tmux_curr_pane_id)
+  tmux setenv $server_name $pane_id
+}
+
+dispatcher_get_pane_id(){
+  server_name=$1
+  tmux showenv $server_name 2>/dev/null | sed "s:^.*=::"
 }
 
 # $1: server name
 dispatcher_take_server(){
-  server=$(tmux showenv ${1} | sed "s:^.*=::")
-	server=$(tmux_take_pane $server)
-	echo dispatcher takes $server
-	if [ server == "-1.-1" ]; then
-		return
-	fi
-
-	tmux setenv $1 $server
+  server_name=$1
+  pane_id=$(dispatcher_get_pane_id $server_name)
+  tmux_take_pane $pane_id
 }
 
 
 # $1: server, $2: command
 dispatcher_send_to() {
-  # TODO: check server exists
-  server=$(tmux showenv ${1} | sed "s:^.*=::")
+  pane_id=$(dispatcher_get_pane_id $1)
   shift
-  tmux send-keys -t $server "$*" Enter
+  tmux send-keys -t $pane_id "$*" Enter
 }
 
 
 dispatcher_usage(){
   echo "USAGE: Set up a server and send command to it."
-	echo "       ex) dispatch_set_server XXX 1.3 (pane id is optional)"
+  echo "       ex) dispatch_set_server XXX 1.3 (pane id is optional)"
   echo "           dispatch_send_to XXX ls"
 }
 
@@ -81,19 +78,23 @@ serial_set_server() {
   dispatcher_set_server ${SERIAL_SERVER_NAME} "$@"
 }
 
+serial_show() {
+  dispatcher_take_server ${SERIAL_SERVER_NAME}
+}
+
 serial_fast() {
   serial_send fast
 }
 
 serial_show_regulators(){
   serial_send \
-		'cd /sys/class/regulator'
+    'cd /sys/class/regulator'
 
   serial_send \
-		'echo "No Name min-voltage target-voltage max-voltage";i=3; while [ 1 ]; do \
-		if [ $i -gt 43 ]; then break; fi; echo "$i: $(cat ./regulator.$i/name) $(cat \
-			./regulator.$i/min_microvolts) <- $(cat ./regulator.$i/microvolts) -> \
-			$(cat ./regulator.$i/max_microvolts)"; i=$(($i+1)); done'
+    'echo "No Name min-voltage target-voltage max-voltage";i=3; while [ 1 ]; do \
+    if [ $i -gt 43 ]; then break; fi; echo "$i: $(cat ./regulator.$i/name) $(cat \
+    ./regulator.$i/min_microvolts) <- $(cat ./regulator.$i/microvolts) -> \
+    $(cat ./regulator.$i/max_microvolts)"; i=$(($i+1)); done'
 }
 
 # usage
