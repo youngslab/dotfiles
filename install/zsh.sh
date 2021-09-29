bfa9ac70549f62ee81e835b232126938326efa68
Jaeyoung Park <jaeyoungs.park@gmail.com>
2021-09-29 11:29:14 +0900

    zsh) theme refined faster

--- install/zsh.sh
+++ install/zsh.sh
@@ -4,10 +4,12 @@ sudo apt install -y zsh autojump
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Oh my zsh plugin 
+# Oh my zsh plugin
 # syntax highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # auto suggestion
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# make it faster
+ln -sfv "$DOTFILES_DIR/runcom/refined_fast.zsh-theme" ~/.oh-my-zsh/themes/refined_fast.zsh-theme
